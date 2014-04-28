1a0bc729c8a5b040b41a90448cbf42cfe894054d
Rick Fletcher <fletch@pobox.com>
2014-04-28 11:48:08 -0400

    Iniital import

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+# == Class: tree
+#
+# Installs (or removes) Tree
+#
+# === Parameters
+#
+# [*ensure*]
+#   Any of the typical $ensure values for a Package: present, absent,
+#   latest, etc.
+#
+# === Examples
+#
+#  class { 'tree':
+#    ensure => latest,
+#  }
+#
+# === Authors
+#
+# Rick Fletcher <fletch@pobox.com>
+#
+# === Copyright
+#
+# Copyright 2014 Rick Fletcher
+#
+class tree (
+  $ensure = 'present',
+) {
+  package { 'tree':
+    ensure => $ensure,
+  }
+}
